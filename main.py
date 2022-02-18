06cfeb1afc19ac0b3cb783643eb9d195533052ac
Eduardo Ismael <eduardo78d@gmail.com>
2022-02-18 10:38:29 -0600

    First commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import toga
+
+def button_handler(widget):
+    print("hello")
+
+
+def build(app):
+    box = toga.Box()
+
+    button = toga.Button('Hello world', on_press=button_handler)
+    button.style.padding = 50
+    button.style.flex = 1
+    box.add(button)
+
+    return box
+
+
+def main():
+    return toga.App(
+        'First App',
+        'org.beeware.helloworld',
+        author='Tiberius Yak',
+        description="A testing app",
+        version=1,
+        home_page="https://beeware.org",
+        startup=build
+    )
+
+
+if __name__ == '__main__':
+    main().main_loop()
